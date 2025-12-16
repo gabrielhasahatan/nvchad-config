3593cf61cba8f12780a8f05dea56b408090bcdc7
gabrielhasahatan <151128430+gabrielhasahatan@users.noreply.github.com>
2025-12-16 08:46:58 +0700

    add relative number

--- init.lua
+++ init.lua
@@ -31,7 +31,6 @@ dofile(vim.g.base46_cache .. "statusline")
 
 require "options"
 require "autocmds"
-
 vim.schedule(function()
   require "mappings"
 end)
--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,5 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+vim.opt.relativenumber = true
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#F0F0F0" })
