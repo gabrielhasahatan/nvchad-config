4edcdb54b56656fd51ee3d50c6a6f350471cd752
gabrielhasahatan <151128430+gabrielhasahatan@users.noreply.github.com>
2026-03-18 23:17:20 +0700

    add shortcut for tsc and copy file path

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,7 +3,9 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
-
+map("n", "<leader>pc", "<cmd> !pnpm tsc --noEmit <cr>", { desc = "Typescript Check" })
+map("n", "<leader>lr", "<cmd> LspRestart <cr>", { desc = "Restart LSP" })
+map("n", "<leader>cr", "<cmd> Cppath <cr>", { desc = "Copy File Relative Path" })
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
